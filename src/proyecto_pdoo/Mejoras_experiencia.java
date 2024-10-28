08d57f8698c61b2df01dceee6c9bac1378432715
DaniEgeaCortacero <daniegeacortacero@gmail.com>
2024-10-28 21:47:45 +0100

    Clases realizadas?

---
+++ src/proyecto_pdoo/Mejoras_experiencia.java
@@ -0,0 +1,82 @@
+package proyecto_pdoo;
+
+public class Mejoras_experiencia extends Mejoras{
+
+    private int increaseExp;
+
+    //----------------------------------- CONSTRUCTORES ---------------------------------------------//
+
+    /**
+     * Constructor por defecto
+     */
+    public Mejoras_experiencia(){
+        this("", "", "", 0, null, 0);
+    }
+
+    /**
+     * Constructor por parámetros
+     * @param name String
+     * @param description String
+     * @param pathImage String
+     * @param prize int
+     * @param obj ObjetoClicker
+     * @param increaseExp int
+     */
+    public Mejoras_experiencia(String name, String description, String pathImage, int prize, ObjetoClicker obj, int increaseExp) {
+        super(name, description, pathImage, prize, obj);
+        this.increaseExp = increaseExp;
+    }
+
+    /**
+     * Constructor de copia
+     * @param otro Mejoras_monedas
+     */
+    public Mejoras_experiencia(Mejoras_experiencia otro){
+        this(otro.getName(), otro.getDescription(), otro.getPathImage(), otro.getPrize(), otro.getObj(), otro.increaseExp);
+    }
+
+
+    //----------------------------------- Getter y setters ---------------------------------------------//
+
+    /**
+     * Getter increaseCoins
+     * @return int
+     */
+    public int getIncreaseExp() {
+        return increaseExp;
+    }
+
+    /**
+     * Setter increasecoins
+     * @param increaseExp int
+     */
+    public void setIncreaseExp(int increaseExp) {
+        this.increaseExp = increaseExp;
+    }
+
+
+    //----------------------------------- FUNCIONES ---------------------------------------------//
+
+    //------ Añade experiencia al total.
+
+    /**
+     * Método addExp.
+     * Suma la experiencia actual con la del parámetro.
+     */
+    public void addExp(){
+        super.getObj().getExp().addExp(this.increaseExp);
+    }
+
+
+    //----------------------------------- OTROS ---------------------------------------------//
+
+
+    @Override
+    public String toString() {
+        return  "Name=" + super.getName() +
+                ", description= " + super.getDescription() +
+                ", pathImage= " + super.getPathImage() +
+                ", prize= " + super.getPrize() +
+                ", increaseExp= " + this.increaseExp;
+    }
+}
---
+++ src/proyecto_pdoo/Mejoras_monedas.java
@@ -0,0 +1,94 @@
+package proyecto_pdoo;
+
+public class Mejoras_monedas extends Mejoras{
+
+    private int increaseCoins;
+
+    //----------------------------------- CONSTRUCTORES ---------------------------------------------//
+
+    /**
+     * Constructor por defecto
+     */
+    public Mejoras_monedas(){
+        this("", "", "", 0, null, 0);
+    }
+
+    /**
+     * Constructor por parámetros
+     * @param name String
+     * @param description String
+     * @param pathImage String
+     * @param prize int
+     * @param obj ObjetoClicker
+     * @param increaseCoins int
+     */
+    public Mejoras_monedas(String name, String description, String pathImage, int prize, ObjetoClicker obj, int increaseCoins) {
+        super(name, description, pathImage, prize, obj);
+        this.increaseCoins = increaseCoins;
+    }
+
+    /**
+     * Constructor de copia
+     * @param otro Mejoras_monedas
+     */
+    public Mejoras_monedas(Mejoras_experiencia otro){
+        this(otro.getName(), otro.getDescription(), otro.getPathImage(), otro.getPrize(), otro.getObj(), otro.getIncreaseExp());
+    }
+
+
+    //----------------------------------- Getter y setters ---------------------------------------------//
+
+    /**
+     * Getter increaseCoins
+     * @return int
+     */
+    public int getIncreaseCoins() {
+        return increaseCoins;
+    }
+
+    /**
+     * Setter increasecoins
+     * @param increaseCoins int
+     */
+    public void setIncreaseCoins(int increaseCoins) {
+        this.increaseCoins = increaseCoins;
+    }
+
+
+    //----------------------------------- FUNCIONES ---------------------------------------------//
+
+
+    //------ Añadir o quitar monedas totales.
+
+    /**
+     * addCoins
+     * Añade o quita monedas del total según el parámetro.
+     */
+    public void addCoins(){
+        super.getObj().getMonedas().addCoins(this.increaseCoins);
+    }
+
+
+    //------ Añadir monedas por click
+
+    /**
+     * addCoinsClick
+     * Suma las monedas por clic actuales con el parámetro.
+     */
+    public void addCoinsClick(){
+        super.getObj().getMonedas().addCoinsClick(this.increaseCoins);
+    }
+
+
+    //----------------------------------- OTROS ---------------------------------------------//
+
+
+    @Override
+    public String toString() {
+        return  "Name=" + super.getName() +
+                ", description= " + super.getDescription() +
+                ", pathImage= " + super.getPathImage() +
+                ", prize= " + super.getPrize() +
+                ", increaseCoins= " + this.increaseCoins;
+    }
+}
